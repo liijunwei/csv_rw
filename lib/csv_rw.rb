47704091a83c090f1fd476f66bc43bf9692a3e9d
lijunwei <ljw532344863@sina.com>
2022-12-29 17:36:06 +0800

    bundle gem csv_rw

---
+++ lib/csv_rw.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "csv_rw/version"
+
+module CsvRw
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/csv_rw_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe CsvRw do
+  it "has a version number" do
+    expect(CsvRw::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
