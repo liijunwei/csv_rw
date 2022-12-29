9539f1b4d097bad552abb937708254b55dad78e3
lijunwei <ljw532344863@sina.com>
2022-12-29 17:43:37 +0800

    Add csv reader & writer tool

--- lib/csv_rw.rb
+++ lib/csv_rw.rb
@@ -3,6 +3,23 @@
 require_relative "csv_rw/version"
 
 module CsvRw
-  class Error < StandardError; end
-  # Your code goes here...
+  def self.read(csv_filepath)
+    result = []
+    CSV.foreach(csv_filepath, headers: true, converters: :all) do |row|
+      result << row.to_h.transform_keys(&:to_sym)
+    end
+
+    result.reject { |e| e.compact.empty? }
+  end
+
+  def self.write(output, csv_filepath)
+    headers = output.first.keys
+
+    CSV.open(csv_filepath, 'w') do |csv|
+      csv << headers
+
+      output.each { |hash| csv << hash.values }
+    end
+  end
 end
+
